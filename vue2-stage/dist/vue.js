2be4673a5ac4ec8310ba77fbe5a84d4a3c58bffc
MineMine <gaoyuan_flying@163.com>
2025-10-28 00:11:09 +0800

    feat:初步实现 computed

--- vue2-stage/dist/vue.js
+++ vue2-stage/dist/vue.js
@@ -281,18 +281,39 @@
     if (opts.data) {
       initData(vm);
     }
+    if (opts.computed) {
+      initComputed(vm);
+    }
+  }
+  function initComputed(vm) {
+    var computed = vm.$options.computed;
+    for (var key in computed) {
+      var computedValue = computed[key];
+      defineComputed(vm, key, computedValue);
+    }
+  }
+  function defineComputed(target, key, computedValue) {
+    // 有可能是对象，有可能是函数
+    var getter = typeof computedValue === 'function' ? computedValue : computedValue.get;
+    var setter = computedValue.set || function () {};
+
+    // 可以通过实例拿到对应的属性
+    Object.defineProperty(target, key, {
+      get: getter,
+      set: setter
+    });
   }
   function initData(vm) {
     var data = vm.$options.data; // data 可能是函数和对象
 
-    data = typeof data === "function" ? data.call(this) : data;
+    data = typeof data === 'function' ? data.call(this) : data;
     vm._data = data;
     // 对数据进行接触，vue2中采用了一个api： defineProperty
     observe(data);
 
     // 将 vm._data 用 vm 来代理就可以了
     for (var key in data) {
-      proxy(vm, "_data", key);
+      proxy(vm, '_data', key);
     }
   }
   function proxy(vm, target, key) {
--- vue2-stage/src/state.js
+++ vue2-stage/src/state.js
@@ -1,35 +1,62 @@
-import { observe } from "./observe/index";
+import { observe } from './observe/index'
 
 export function initState(vm) {
-  const opts = vm.$options;
+  const opts = vm.$options
 
   if (opts.data) {
-    initData(vm);
+    initData(vm)
+  }
+
+  if (opts.computed) {
+    initComputed(vm)
   }
 }
 
+function initComputed(vm) {
+  const computed = vm.$options.computed
+
+  for (let key in computed) {
+    let computedValue = computed[key]
+
+    defineComputed(vm, key, computedValue)
+  }
+}
+
+function defineComputed(target, key, computedValue) {
+  // 有可能是对象，有可能是函数
+  const getter =
+    typeof computedValue === 'function' ? computedValue : computedValue.get
+  const setter = computedValue.set || (() => {})
+
+  // 可以通过实例拿到对应的属性
+  Object.defineProperty(target, key, {
+    get: getter,
+    set: setter,
+  })
+}
+
 function initData(vm) {
-  let data = vm.$options.data; // data 可能是函数和对象
+  let data = vm.$options.data // data 可能是函数和对象
 
-  data = typeof data === "function" ? data.call(this) : data;
+  data = typeof data === 'function' ? data.call(this) : data
 
-  vm._data = data;
+  vm._data = data
   // 对数据进行接触，vue2中采用了一个api： defineProperty
-  observe(data);
+  observe(data)
 
   // 将 vm._data 用 vm 来代理就可以了
   for (const key in data) {
-    proxy(vm, "_data", key);
+    proxy(vm, '_data', key)
   }
 }
 
 function proxy(vm, target, key) {
   Object.defineProperty(vm, key, {
     get() {
-      return vm[target][key];
+      return vm[target][key]
     },
     set(val) {
-      vm[target][key] = val;
+      vm[target][key] = val
     },
-  });
+  })
 }
