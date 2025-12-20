9ed767611cd29ca63123ca4b4210712ba7a4b7f7
MineMine <gaoyuan_flying@163.com>
2025-12-20 17:56:33 +0800

    feat:项目搭建已经初始化

---
+++ zhufeng-ketang/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    hello
+    <nav>
+      <router-link to="/">Home</router-link>
+      |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  color: $color
+}
+</style>
---
+++ zhufeng-ketang/vue.config.js
@@ -0,0 +1,32 @@
+const {defineConfig} = require('@vue/cli-service')
+const path = require('path')
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [
+        path.resolve(__dirname, "src/assets/common.scss")
+      ]
+    }
+  },
+
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          require("postcss-plugin-px2rem")({
+            rootValue: 75, // 设计稿宽度/10，如750设计稿就是75
+            propList: ['*'], // 需要转换的属性，*表示所有
+            selectorBlackList: [], // 不转换的选择器
+            replace: true,
+            mediaQuery: false,
+            minPixelValue: 0
+          })
+        ]
+      }
+    }
+  }
+})
